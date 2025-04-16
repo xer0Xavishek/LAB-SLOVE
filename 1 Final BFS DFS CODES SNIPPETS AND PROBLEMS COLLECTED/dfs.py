25782cf6d93723499a228dbc635f756a8b4ac91c
Avishek Biswas <avishek.biswas@g.bracu.ac.bd>
2025-04-16 10:38:12 +0600

    again v2 modification of the code

--- 1 Final BFS DFS CODES SNIPPETS AND PROBLEMS COLLECTED/dfs.py
+++ 1 Final BFS DFS CODES SNIPPETS AND PROBLEMS COLLECTED/dfs.py
@@ -4,38 +4,35 @@ def dfs(graph):
     discovery = {}
     finish = {}
     traversal_order = []
+    time = {'value': 0}  # Use a dictionary to manage time as a shared mutable object
 
     # Initialization
     for u in graph:
         color[u] = 'WHITE'
         predecessor[u] = None
 
-    time = 0  # Use an integer for time
-
     # DFS
     for u in graph:
         if color[u] == 'WHITE':
-            time = dfs_visit(graph, u, color, predecessor, discovery, finish, time, traversal_order)
+            dfs_visit(graph, u, color, predecessor, discovery, finish, time, traversal_order)
 
     return discovery, finish, predecessor
 
 
 def dfs_visit(graph, u, color, predecessor, discovery, finish, time, traversal_order):
     color[u] = 'GRAY'
-    time += 1
-    discovery[u] = time
+    time['value'] += 1
+    discovery[u] = time['value']
 
     for v in graph[u]:
         if color[v] == 'WHITE':
             predecessor[v] = u
-            time = dfs_visit(graph, v, color, predecessor, discovery, finish, time, traversal_order)
+            dfs_visit(graph, v, color, predecessor, discovery, finish, time, traversal_order)
 
     color[u] = 'BLACK'
-    time += 1
-    finish[u] = time
+    time['value'] += 1
+    finish[u] = time['value']
     traversal_order.append(u)
-
-    return time  # Return the updated time
 
 
 # Example graph
